0f938b45bf0459fabfb13de5c2af74ef747d0736
derek <44205539+derekharrison@users.noreply.github.com>
2022-12-13 09:23:45 +0100

    do not pause game when hit bird and lives > 0.
    To keep thread alive for game continue/restart.

---
+++ java/com/main/guapogame/Flag.java
@@ -0,0 +1,11 @@
+package com.main.guapogame;
+
+import android.content.res.Resources;
+
+public class Flag extends Popup {
+
+    public Flag(Resources res, int screenFactorX, int screenFactorY, int image_id) {
+
+        super(res, screenFactorX, screenFactorY, image_id);
+    }
+}
