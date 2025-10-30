98c5ab501600152e41469d2a530e634c2c3964a9
derek <44205539+derekharrison@users.noreply.github.com>
2025-10-30 20:36:37 +0100

    Delete guapo-game-android/java directory

--- guapo-game-android/java/GameImage.java
+++
@@ -1,7 +0,0 @@
-package com.main.guapogame;
-
-import android.graphics.Bitmap;
-
-public interface GameImage {
-    Bitmap getImage();
-}
--- guapo-game-android/java/java/Velocity.java
+++
@@ -1,7 +0,0 @@
-package com.main.guapogame;
-
-public interface Velocity {
-
-    float getVelocityX();
-    float getVelocityY();
-}
