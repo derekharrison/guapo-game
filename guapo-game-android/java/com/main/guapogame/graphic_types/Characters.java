0b6dd13ddab0ee15a2b1e58994b9b69d75e9a5fa
derek <44205539+derekharrison@users.noreply.github.com>
2025-11-05 22:15:23 +0100

    Delete guapo-game-android/java/com/main/guapogame directory

--- guapo-game-android/java/com/main/guapogame/graphic_types/Characters.java
+++
@@ -1,9 +0,0 @@
-package com.main.guapogame.graphic_types;
-
-// TODO : introduce new characters
-
-public enum Characters {
-    FRITO,
-    MICA,
-    CHIVAS
-}
--- guapo-game-android/java/com/main/guapogame/resources/Heros.java
+++
@@ -1,6 +0,0 @@
-package com.main.guapogame.resources;
-
-public enum Heros {
-    TUTTI,
-    GUAPO
-}
