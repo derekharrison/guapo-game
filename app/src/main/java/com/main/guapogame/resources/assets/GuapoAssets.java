e2265bfb116e16f8b6cac045fd6015a02705fb1d
Derek <harrison.derek.w@gmail.com>
2026-02-22 15:57:51 +0100

    Add guapo assets for character following popup

---
+++ app/src/main/java/com/main/guapogame/resources/assets/GuapoAssets.java
@@ -0,0 +1,19 @@
+package com.main.guapogame.resources.assets;
+
+import com.main.guapogame.R;
+
+public class GuapoAssets {
+    public static Integer getImage() {
+        return R.drawable.guapo_main_image_bitmap_cropped;
+    }
+
+    public static Integer getImageHit() {
+        return R.drawable.guapo_main_image_hit_bitmap_cropped;
+    }
+
+    public static Integer getCapeImage1() { return R.drawable.cape1_bitmap_cropped1_green; }
+
+    public static Integer getCapeImage2() { return R.drawable.cape2_bitmap_cropped1_green; }
+
+    private GuapoAssets() {}
+}
